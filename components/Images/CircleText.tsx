08fa2397d3f02fa6a06586be07959d5fd5e8257a
PolarisButReal <morgan@morganhofmann.nl>
2022-08-27 19:28:31 +0200

    Overflow issues fixed, circular text recentered

--- components/Images/CircleText.tsx
+++ components/Images/CircleText.tsx
@@ -1,17 +1,17 @@
-import * as React from "react"
+import * as React from "react";
 
 type Props = {
-    width?: number | string;
-    height?: number | string;
-    opacity?: number;
-}
+  width?: number | string;
+  height?: number | string;
+  opacity?: number;
+};
 
 const SvgComponent = ({ width = 300, height = 300, opacity = 1.0 }: Props) => (
   <svg
     baseProfile="full"
     width={width}
     height={height}
-    viewBox="0 0 320 320"
+    viewBox="0 0 280 280"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
@@ -27,17 +27,22 @@ const SvgComponent = ({ width = 300, height = 300, opacity = 1.0 }: Props) => (
         }
       }
     `}</style>
+
     <g opacity={opacity}>
       <text stroke="#fff" fill="#fff" fontSize={17.6} fontWeight="normal">
-        <textPath path="M 20,140
+        <textPath
+          path="M 20,140
           a 120,120 0 1,0 240,0
           a 120,120 0 1,0 -240,0"
-          method="stretch" xlinkHref="#curve">
-          THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY
+          method="stretch"
+          xlinkHref="#curve"
+        >
+          THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY THE OPEN SOURCE
+          COMPANY
         </textPath>
       </text>
     </g>
   </svg>
-)
+);
 
-export default SvgComponent
+export default SvgComponent;
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <Layout>
       <div className="background flex-center">
-        <CircleText width="600" height="600" opacity={0.2} />
+        <CircleText width="600" height="600" opacity={0.1} />
       </div>
       <div className="foreground">
         <div className={styles.container}>
@@ -20,12 +20,13 @@ export default function About() {
             quis auctor nulla. Duis sollicitudin sit amet quam at placerat.
             Vivamus congue mi justo, vitae ultrices velit hendrerit a. Nulla
             lectus lectus, pharetra quis congue quis, maximus eu ipsum. Sed quis
-            pulvinar eros. Vestibulum aliquet ex et diam tempor luctus. Phasellus
-            quis erat sed libero porttitor mollis. Proin ullamcorper nunc in
-            ligula faucibus maximus. Proin blandit ornare convallis. Fusce
-            tincidunt sit amet risus et fermentum. Donec aliquam nulla nulla, ac
-            condimentum ligula mollis nec. Maecenas non odio non tellus suscipit
-            mattis. Integer ac enim viverra, pellentesque diam ut, mollis nunc.
+            pulvinar eros. Vestibulum aliquet ex et diam tempor luctus.
+            Phasellus quis erat sed libero porttitor mollis. Proin ullamcorper
+            nunc in ligula faucibus maximus. Proin blandit ornare convallis.
+            Fusce tincidunt sit amet risus et fermentum. Donec aliquam nulla
+            nulla, ac condimentum ligula mollis nec. Maecenas non odio non
+            tellus suscipit mattis. Integer ac enim viverra, pellentesque diam
+            ut, mollis nunc.
           </p>
         </div>
       </div>
