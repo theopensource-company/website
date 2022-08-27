d80b4e2de2c3288c228d2ac1588687eb6d2fc4ff
PolarisButReal <morgan@morganhofmann.nl>
2022-08-27 19:12:04 +0200

    Stylistic changes, light/dark theme compatibility
    Also scaled up the SVG so the text is less messy, and added an opacity option for reusability.

--- components/Images/CircleText.tsx
+++ components/Images/CircleText.tsx
@@ -3,25 +3,40 @@ import * as React from "react"
 type Props = {
     width?: number | string;
     height?: number | string;
+    opacity?: number;
 }
 
-const SvgComponent = ({ width = 300, height = 300 }: Props) => (
+const SvgComponent = ({ width = 300, height = 300, opacity = 1.0 }: Props) => (
   <svg
     baseProfile="full"
     width={width}
     height={height}
-    viewBox="0 0 160 160"
+    viewBox="0 0 320 320"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
-    <text stroke="#fff" fill="#fff" fontSize={8.8} fontWeight="normal">
-      <textPath path="M 20,80
-        a 60,60 0 1,0 120,0
-        a 60,60 0 1,0 -120,0"
-        method="stretch" xlinkHref="#curve">
-        THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY
-      </textPath>
-    </text>
+    <style>{`
+      text {
+        fill: black;
+        stroke: black;
+      }
+      @media (prefers-color-scheme: dark) {
+        text {
+          fill: white;
+          stroke: white;
+        }
+      }
+    `}</style>
+    <g opacity={opacity}>
+      <text stroke="#fff" fill="#fff" fontSize={17.6} fontWeight="normal">
+        <textPath path="M 20,140
+          a 120,120 0 1,0 240,0
+          a 120,120 0 1,0 -240,0"
+          method="stretch" xlinkHref="#curve">
+          THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY THE OPEN SOURCE COMPANY
+        </textPath>
+      </text>
+    </g>
   </svg>
 )
 
