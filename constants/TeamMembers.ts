36bc66277cb876a65591196c84ad92fdb1115a82
Hesselaar23 <81434106+Hesselaar23@users.noreply.github.com>
2022-10-20 20:10:52 +0200

    New person (#12)
    Added Hessel van Bakel to the team

--- constants/TeamMembers.ts
+++ constants/TeamMembers.ts
@@ -3,6 +3,7 @@ import { StaticImageData } from 'next/image';
 import PortraitMicha from '../assets/image/team/m-d-vries.jpg';
 import PortraitMorgan from '../assets/image/team/m-hofmann.jpg';
 import PortraitTies from '../assets/image/team/t-wellhuner.jpg';
+import PortraitHessel from '../assets/image/team/h-v-bakel.jpg';
 
 export const TeamMembers: Array<{
     name: string;
@@ -24,4 +25,10 @@ export const TeamMembers: Array<{
         portrait: PortraitTies,
         role: "Product, Graphic Design"
     },
+    {
+        name: "Hessel van Bakel",
+        portrait: PortraitHessel,
+        role: "Product, Graphic Design"
+    },
+
 ];
