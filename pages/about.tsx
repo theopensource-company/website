cc95759259002550c7547d70d8b598fcdb76c84f
Micha de Vries <micha@devrie.sh>
2023-02-18 12:12:04 +0100

    Update about page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import CircleText from "../components/Images/CircleText";
 import TeamMember from "../components/About/TeamMember";
 import { TeamMembers } from "../constants/TeamMembers";
 import styles from "../styles/About.module.scss";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -16,10 +17,13 @@ export default function About() {
           <section>
             <h1>Trust Through Vision, Not Through Deceit</h1>
             <p>
-              We believe in open source software and its endless possibilities.
-              We believe in peeling data harvesting and unetchical
-              advertisements off the internet. <br />
-              <br />
+              TOSC, short for &quot;The Open Source Company&quot;, is a
+              collective of enthusiastic software developers that want to make a
+              change.
+              <br /> <br />
+              We believe in open source projects and transparent communication.
+              We see it as one big collaboration, instead of competition.
+              <br /> <br />
               Join us on our mission, our contribution to the world.
             </p>
           </section>
@@ -28,6 +32,17 @@ export default function About() {
             <div className={styles.teamContainer}>
               {TeamMembers.map((member, i) => TeamMember(member, i))}
             </div>
+            <p
+              style={{
+                fontSize: "1.2rem",
+              }}
+            >
+              Want to join us? Find us on{" "}
+              <Link href="https://discord.gg/DRCV4g7Bmy" target="_blank">
+                Discord
+              </Link>{" "}
+              :)
+            </p>
           </section>
         </div>
       </div>
